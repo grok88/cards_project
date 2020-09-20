3c931e395100dadb6b3f96fd4b71eaf5743f6794
Alex <grok88@tut.by>
2020-09-20 19:08:38 +0300

    added to Login HTML plug

--- src/n2-features/f1-auth/a1-login/l1-ui/Login.tsx
+++ src/n2-features/f1-auth/a1-login/l1-ui/Login.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
-export const Login = () => {
-    return <div>
-        LOGIN
-    </div>
+type LoginPropsType = {
+
+}
+
+export const Login:React.FC<LoginPropsType> = () => {
+    return (
+        <div style={{width:'40%', outline:'1px solid red', margin:'0 auto', display:'flex', flexDirection:"column", justifyContent:"center", alignItems:'center'}}>
+            <h2>Login</h2>
+            <div>
+                <input type="text" placeholder={'enter you name'}/>
+            </div>
+            <div>
+                <input type="text" placeholder={'enter you name'}/>
+            </div>
+            <div>
+                <label >
+                    Remember Me
+                    <input type="checkbox" name="check"/>
+                </label>
+            </div>
+            <div>
+                <a href="#">forgot?</a>
+            </div>
+            <button>Sign in</button>
+           <div>
+               <a href="#">Register</a>
+           </div>
+        </div>
+    );
 }
